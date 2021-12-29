9d53185f38eeeb4dc6ea18229361c4c6abb68a91
nhanlnt-96 <nhanlnt@hotmail.com>
2021-12-30 00:20:40 +0700

    [about]: Complete create abount component

---
+++ src/configs/aboutUseData.js
@@ -0,0 +1,9 @@
+export const aboutUseData = ["MetaVS takes place in the world of NeoRen, a futuristic planet divided into different" +
+" factions,and cities - each faction offers different types of enemies, resources and quests to challenge players" +
+" and help them level up their skills. Players can travel throughout NeoRen via a number of methods including on" +
+" foot, hovercraft, or spaceships all of which will be able to be either purchased directly from our MetaVS NFT" +
+" marketplace or minted once a player has acquired all necessary NFT parts which can be earned in game or bought" +
+" from the NFT marketplace.", "Players are represented in the game with customisable avatars which they will create" +
+" once joining, MetaMen and MetaWomen holders can choose to have their NFTs as their avatar.", "MetaVS does not" +
+" follow a linear storyline; rather, players set their own goals and objectives.", "Players can choose to fight" +
+" non-player character (NPC) monsters, complete quests, or increase their experience in the available skills.", "Players interact with each other through trading, chatting, or by participating in our PvP arena battles where players can battle to the death whilst earning."]
--- src/page/MainLayout.js
+++ src/page/MainLayout.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Container, Row} from "react-bootstrap";
 import BannerComp from "../components/banner/BannerComp";
+import AboutComp from "../components/about/AboutComp";
 
 const MainLayout = () => {
   return (
     <Container fluid className="main-layout">
       <Row>
         <BannerComp/>
+      </Row>
+      <Row>
+        <AboutComp/>
       </Row>
     </Container>
   );
