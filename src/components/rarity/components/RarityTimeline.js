b62b6045ff38a05bd5500bef41204b1f6578dee5
nhanlnt-96 <nhanlnt@hotmail.com>
2022-01-01 01:21:47 +0700

    [rarity]: Update rarity timeline component

--- src/components/rarity/components/RarityTimeline.js
+++ src/components/rarity/components/RarityTimeline.js
@@ -1,52 +1,35 @@
 import React from "react";
 import {Col, Container, Row} from "react-bootstrap";
+import {rarityTimelineData} from "../../../configs/rarityTimelineData";
 
 export const RarityTimeline = () => {
   return (
     <Row className="rarity-comp-timeline">
       <Container className="rarity-comp-timeline-container">
         <Row className="items">
-          <Col lg={3} md={6} sm={12} className="item">
-            <div className="item-header" style={{backgroundColor: "#f14b1f"}}>
-              <h6 className="title">Q1<br/>2022</h6>
-            </div>
-            <div className="item-content">
-              <p className="content">MetaMan NFT Launch</p>
-            </div>
-          </Col>
-          <Col lg={3} md={6} sm={12} className="item-empty"/>
-          <Col lg={3} md={6} sm={12} className="item">
-            <div className="item-header" style={{backgroundColor: "#5eb090"}}>
-              <h6 className="title">Q3<br/>2022</h6>
-            </div>
-            <div className="item-content">
-              <p className="content">MetaWomen NFT Launch</p>
-              <p className="content">MetaVS Game V1 Release - PvP Arena Battle Play-To-Earn Game</p>
-            </div>
-          </Col>
-          <Col lg={3} md={6} sm={12} className="item-empty"/>
-        </Row>
-        <Row className="items">
-          <Col lg={3} md={6} sm={12} className="item-empty"/>
-          <Col lg={3} md={6} sm={12} className="item">
-            <div className="item-header" style={{backgroundColor: "#5271ff"}}>
-              <h6 className="title">Q2<br/>2022</h6>
-            </div>
-            <div className="item-content">
-              <p className="content">NFT Marketplace Launch</p>
-              <p className="content">MetaVS Token Launch</p>
-              <p className="content">Staking Launch</p>
-            </div>
-          </Col>
-          <Col lg={3} md={6} sm={12} className="item-empty"/>
-          <Col lg={3} md={6} sm={12} className="item">
-            <div className="item-header" style={{backgroundColor: "#9d5eb0"}}>
-              <h6 className="title">Q1<br/>2022/2023</h6>
-            </div>
-            <div className="item-content">
-              <p className="content">MetaVS Game V2 Release - RPG MMO Play-To-Earn Game</p>
-            </div>
-          </Col>
+          {
+            rarityTimelineData.map((val, index) => (
+              <div key={index} className="item-container">
+                <div className="item-child d-flex flex-column justify-content-center align-items-center">
+                  <div className="header d-flex flex-column justify-content-center align-items-center"
+                       style={{backgroundColor: val.color}}>
+                    {
+                      val.title.map((title, titleKey) => (
+                        <h6 key={titleKey} className="title">{title}</h6>
+                      ))
+                    }
+                  </div>
+                  <div className="content d-flex flex-column justify-content-center align-items-center">
+                    {
+                      val.content.map((content, key) => (
+                        <p key={key} className="detail">{content}</p>
+                      ))
+                    }
+                  </div>
+                </div>
+              </div>
+            ))
+          }
         </Row>
       </Container>
     </Row>
---
+++ src/configs/rarityTimelineData.js
@@ -0,0 +1,22 @@
+export const rarityTimelineData = [
+  {
+    title: ["Q1", "2022"],
+    content: ["MetaMan NFT Launch"],
+    color: "#f14b1f"
+  },
+  {
+    title: ["Q2", "2022"],
+    content: ["NFT Marketplace Launch", "MetaVS Token Launch", "Staking Launch"],
+    color: "#5271ff"
+  },
+  {
+    title: ["Q3", "2022"],
+    content: ["MetaWomen NFT Launch", "MetaVS Game V1 Release - PvP", "Arena Battle Play-To-Earn Game"],
+    color: "#5eb090"
+  },
+  {
+    title: ["Q1", "2022/2023"],
+    content: ["MetaVS Game V2 Release - RPG MMO Play-To-Earn Game"],
+    color: "#9d5eb0"
+  }
+]
